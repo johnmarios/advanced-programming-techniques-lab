954e50c1e17e502380c6a365cfc7515d8c7d9639
Filipposth <filippos.theologos@gmail.com>
2026-05-10 22:46:38 +0300

    debug=false

--- labs/lab08/api.py
+++ labs/lab08/api.py
@@ -928,8 +928,7 @@ class MQTTTopicDetail(Resource):
 if __name__ == "__main__":
 
     app.run(
-        debug=True,
-        use_reloader=False,
+        debug=False,
         host="0.0.0.0",
         port=5000
     )
