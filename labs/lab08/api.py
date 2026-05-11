637ec8c7c5de637e73e9cee3a433f6b1b7e516f3
Filipposth <filippos.theologos@gmail.com>
2026-05-11 22:18:45 +0300

    api

--- labs/lab08/api.py
+++ labs/lab08/api.py
@@ -199,16 +199,6 @@ def on_message(client, userdata, msg):
             ).isoformat().replace("+00:00", "Z")
         }
 
-mqtt_client.on_message = on_message
-print("CONNECTING MQTT CLIENT")
-mqtt_client.connect("localhost",1883,60)
-
-mqtt_client.subscribe("smartbin/#",qos=1)
-
-mqtt_client.subscribe("environments/#",qos=1)
-
-mqtt_client.loop_start()
-
 
 api = Api(
     app,
@@ -927,10 +917,20 @@ class MQTTTopicDetail(Resource):
 
 if __name__ == "__main__":
 
+    mqtt_client.on_message = on_message
+
+    print("CONNECTING MQTT CLIENT")
+
+    mqtt_client.connect("localhost",1883,60)
+
+    mqtt_client.subscribe("smartbin/#",qos=1)
+    mqtt_client.subscribe("environments/#",qos=1)
+
+    mqtt_client.loop_start()
+
     app.run(
         debug=False,
         use_reloader=False,
         host="0.0.0.0",
         port=5000
     )
-   
